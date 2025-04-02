79735b107821bab5a8c58fd87adb6e6c7991228f
juliopontocom <146362643+juliopontocom@users.noreply.github.com>
2025-04-02 18:03:11 -0300

    fixes and serach

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css'
 import Login from './assets/pages/Login';
+import Search from './assets/pages/Search';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
-        {/* Adicione mais rotas aqui conforme necessário */}
+        <Route path="/search" element={<Search />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/assets/pages/Search/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './styles.module.css';
+
+const Search = () => {
+  return (
+    <div className={styles.container}>
+        <h1 className={styles.title}>Findt</h1>
+        
+      </div>
+  );
+};
+
+export default Search;
